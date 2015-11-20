309149b2824baccbefb7199a28623b68c860d5c9
Michal Štrba <faiface2202@gmail.com>
2015-11-20 22:37:08 +0100

    Hlavne veci v observeri. Chyba samotne vykreslovanie jednotlivych objektov.

--- observer/main.cpp
+++ observer/main.cpp
@@ -1,7 +1,7 @@
-
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
+#include <cmath>
 #include <unistd.h>
 #include <string>
 #include "SDL.h"
@@ -10,10 +10,9 @@ using namespace std;
 
 #include "logic.h"
 
+#define FPS 30
 
-#define INITIAL_SPEED 100
-#define FPS 50
-
+#define FARBA_POZADIA   0x101010
 
 int main(int argc, char *argv[]) {
   if (argc != 2 || argv[1][0] == '-') {
@@ -21,9 +20,11 @@ int main(int argc, char *argv[]) {
     return 0;
   }
 
-  nacitajAdresar(argv[1]);
-
   srand(time(NULL) * getpid());
+
+  string zaznamovyAdresar(argv[1]);
+  Hra hra;
+  hra.nacitajSubor(zaznamovyAdresar + "/zaznam");
 
   SDL_Init(SDL_INIT_VIDEO);
   atexit(SDL_Quit);
@@ -31,71 +32,59 @@ int main(int argc, char *argv[]) {
   TTF_Init();
   atexit(TTF_Quit);
 
-  nacitajMedia();
+  SDL_Surface *screen = SDL_SetVideoMode(1024, 768, 32, SDL_SWSURFACE);
 
-  int w = 1024;
-  int h = 768;
-  const char *fs = getenv("FULLSCREEN");
-  SDL_Surface *screen = SDL_SetVideoMode(w, h, 32, SDL_SWSURFACE | (fs && *fs ? SDL_FULLSCREEN : 0));
-
-  string title = string("Observerr - ") + argv[1];
+  string title = string("Observer - ") + argv[1];
   SDL_WM_SetCaption(title.c_str(), title.c_str());
 
-  int programBegin = SDL_GetTicks();
-  bool paused = false;
-  double pause = 0;
-  double speed = INITIAL_SPEED;
+  double cas = 0.0;   // na ktorom ticku hry sme, samozrejme az po zaokruhleni
+  double rychlost = 1.0;
+  bool pauza = false;
 
   while (true) {
-    int frameBegin = SDL_GetTicks();
-
-    double now = (paused ? pause : (frameBegin - programBegin) / speed);
-    if (now < 0) {
-      pause = now = 0;
-      paused = true;
-      if (speed < 0) speed = -speed;
-    }
-    vykresluj(screen, now);
-    SDL_Flip(screen);
+    int pocetTickovNaZaciatku = SDL_GetTicks();
 
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
       if (event.type == SDL_QUIT) return 1;
-      if (event.type == SDL_KEYUP || event.type == SDL_KEYDOWN) {
-        int sym = event.key.keysym.sym;
-        int mod = (event.key.keysym.mod & (KMOD_NUM - 1));
-        if (event.type == SDL_KEYUP && sym == SDLK_s && (mod == KMOD_LALT || mod == KMOD_RALT)) return 0;
-        if (event.type == SDL_KEYUP && sym == SDLK_q && (mod == KMOD_LALT || mod == KMOD_RALT)) return 1;
-        if (event.type == SDL_KEYDOWN && (sym == SDLK_p || sym == SDLK_SPACE) && mod == 0) {
-          paused = !paused;
-          if (paused) {
-            pause = now;
-          } else {
-            programBegin = frameBegin - (int)(pause * speed);
-          }
-        }
-        if (event.type == SDL_KEYDOWN && sym == SDLK_PERIOD && mod == 0) {
-          if (paused) {
-            pause = 1 + (int)pause;
-          } else {
-            pause = now;
-            paused = true;
-          }
-        }
-        double speedmod = 0;
-        switch (sym) {
-          case SDLK_MINUS: speedmod = 1.25; break;
-          case SDLK_EQUALS: speedmod = 0.8; break;
-          case SDLK_r: speedmod = -1; break;
-        }
-        if (event.type == SDL_KEYUP && speedmod) {
-          speed *= speedmod;
-          if (!paused) programBegin = frameBegin - (int)(now * speed);
+
+      if (event.type == SDL_KEYDOWN) {
+        int key = event.key.keysym.sym;
+
+        switch (key) {
+          case SDLK_p: {
+            pauza = !pauza;
+          } break;
+
+          case SDLK_r: {
+            rychlost *= -1;
+          } break;
+
+          case SDLK_EQUALS:
+          case SDLK_PLUS: {
+            rychlost *= 1.25;
+          } break;
+
+          case SDLK_MINUS: {
+            rychlost *= 0.8;
+          } break;
         }
       }
     }
 
-    Uint32 duration = SDL_GetTicks() - frameBegin;
-    if (duration < 1000/FPS) SDL_Delay(1000/FPS - duration);
+    if (!pauza) cas += rychlost;
+    if (cas < 0) cas = 0;
+    if (cas >= hra.framy.size()) cas = hra.framy.size() - 1;
+
+    int tick = floor(cas);
+
+    SDL_FillRect(screen, &screen->clip_rect, FARBA_POZADIA);    // vycistime obrazovku
+
+    hra.framy[tick].kresli(screen);                             // nakreslime sucasny stav
+
+    SDL_Flip(screen);                                           // zobrazime, co sme nakreslili, na obrazovku
+
+    int pocetTickovNaKonci = SDL_GetTicks();
+    SDL_Delay(1000 / FPS - (pocetTickovNaKonci - pocetTickovNaZaciatku));
   }
 }
